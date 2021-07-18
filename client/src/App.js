a33eb986f303a986a9f441d853ac25db15f8655b
VirajKanse <virajv2k@gmail.com>
2021-07-18 18:09:08 +0530

    Added redux store & alert system Note: Alert system is using toasts to show alerts and not traditional alerts, because toasts seems more useful & makes sense to use it.

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,37 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Landing from './components/layout/Landing';
+import Alert from './components/layout/Alert';
 import Login from './components/auth/Login';
 import Signup from './components/auth/Signup';
+import { Box } from '@chakra-ui/react';
+import { Provider } from 'react-redux';
+import store from './store';
+import { useEffect } from 'react';
+import { loadUser } from './actions/auth';
+import setAuthToken from './utils/setAuthToken';
+
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
 
 function App() {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+
   return (
-    <Router>
-      <Switch>
-        <Route path='/' component={Landing} exact></Route>
-        <Route path='/sign-up' component={Signup}></Route>
-        <Route path='/log-in' component={Login}></Route>
-      </Switch>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Route exact path='/' component={Landing}></Route>
+        <Box as='section'>
+          <Alert />
+          <Switch>
+            <Route exact path='/sign-up' component={Signup}></Route>
+            <Route exact path='/log-in' component={Login}></Route>
+          </Switch>
+        </Box>
+      </Router>
+    </Provider>
   );
 }
 
