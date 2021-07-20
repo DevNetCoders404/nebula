830c18d66d662e04e4f6661c9d67cff694d6797e
VirajKanse <virajv2k@gmail.com>
2021-07-20 19:44:13 +0530

    Added simple loader/spinner animation - Will beutify spinner in future - Set loading bool to true by default in auth reducer

---
+++ client/src/components/layout/Loader.js
@@ -0,0 +1,12 @@
+import { Center } from '@chakra-ui/react';
+import { Spinner } from '@chakra-ui/spinner';
+
+const Loader = () => {
+  return (
+    <Center h='100vh'>
+      <Spinner thickness={["4px", "8px"]} speed='0.65s' emptyColor='gray.200' color='teal.500' size='xl' w={["4rem", "6rem"]} h={["4rem", "6rem"]} />
+    </Center>
+  );
+};
+
+export default Loader;
