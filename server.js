cb368cf8a4b7323cd58fad5d4ed52762e29f7d64
pegasys22 <pegasys.sec@gmail.com>
2021-07-20 20:13:48 +0530

    database connectivity implemented, added db.js

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const connectDB = require('./config/db');
 const app = express();
+
+//Connect the Database
+connectDB();
 
 app.get('/', (req, res) => res.send('API Running'));
 
