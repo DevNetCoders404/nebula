58f8f0b9415d4ef20ee0df9a0ffd08db0fc9cd8d
pega5y5 <yashrajmore22@gmail.com>
2021-07-21 20:23:09 +0530

    implemented basic jwt for user registration - added jwt secret in config and used it in users.js - used jwt sign while registration

--- config/db.js
+++ config/db.js
@@ -6,7 +6,8 @@ const connectDB = async () => {
   try {
     await mongoose.connect(db, {
       useNewUrlParser: true,
-      useUnifiedTopology: true
+      useUnifiedTopology: true,
+      useCreateIndex: true
     });
     //connecting db from mongoURI
     console.log('MongoDB connected...');
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator');
+
+// Importing User Model
+const User = require('../../models/Users');
 
 // @route   POST api/users
 // @desc    Register user
@@ -12,12 +19,57 @@ router.post(
     check('email', 'Please include a valid email').isEmail(),
     check('password', 'Please enter password with 6 or more character').isLength({ min: 6 })
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    res.send('User route');
+
+    const { name, email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+
+      if (user) {
+        return res.status(400).json({ errors: [{ msg: 'User already exists' }] });
+      }
+
+      const avatar = gravatar.url(email, {
+        s: '200',
+        r: 'pg',
+        d: 'mm'
+      });
+
+      user = new User({
+        name,
+        email,
+        avatar,
+        password
+      });
+
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      const payload = {
+        user: {
+          id: user.id
+        }
+      }
+
+      jwt.sign(payload,config.get('jwtSecret'),{
+        expiresIn: 380000
+      }, (err, token) => {
+        if(err) throw err;
+        res.json({token});
+      })
+
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
   }
 );
 
