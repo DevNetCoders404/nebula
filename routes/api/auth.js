da4bb51687a1eba24021691b57ac6d36e9bb0053
pega5y5 <yashrajmore22@gmail.com>
2021-07-21 18:49:09 +0530

    added Request & Body Validation - modified users.js for POST - added Init Middleware

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -4,8 +4,8 @@ const router = express.Router();
 // @route   GET api/auth
 // @desc    Test route
 // @access  Public
-router.get('/',(req, res) => {
+router.get('/', (req, res) => {
   res.send('Auth route');
 });
 
-module.exports = router; 
+module.exports = router;
--- server.js
+++ server.js
@@ -2,8 +2,11 @@ const express = require('express');
 const connectDB = require('./config/db');
 const app = express();
 
-//Connect the Database
+// Connect the Database
 connectDB();
+
+// Init Middleware
+app.use(express.json({ extended: false }));
 
 app.get('/', (req, res) => res.send('API Running'));
 
@@ -11,7 +14,6 @@ app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
-
 
 const PORT = process.env.PORT || 5000;
 
